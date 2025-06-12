70eaead3ea484b6406990fedeef1ae3cfa843d86
preranavichare01 <112305774+preranavichare01@users.noreply.github.com>
2025-06-12 19:54:42 +0530

    Update and rename upload_dataset.csv to upload_dataset.py

--- upload_dataset.py
+++ upload_dataset.py
@@ -10,8 +10,8 @@ collection = client.get_or_create_collection("energial_data")
 datasets = {
     "energy_consumption.csv": lambda row: f"Timestamp: {row['timestamp']}, Building_id: {row['building_id']},Energy_usage: {row['energy_usage']} kWh, Energy_source: {row['energy_source']} , Temperature: {row['temperature']},Humidity: {row['humidity']}, ",
     "energy_generation.csv": lambda row: f"Timestamp: {row['timestamp']} ,Energy_source: {row['energy_source']},,Energy_usage: {row['energy_usage']} kWh, Location: {row['location']} ",
-    "building_information.csv": lambda row: f"Timestamp: {row['timestamp']}, Location: {row['location']}, Temperature: {row['temperature']},Humidity: {row['humidity']}, Solar_irradiance: {row['solar_irradiance']}",
-    "carbon_report.csv": lambda row: f"Timestamp: {row['timestamp']}, Energy_source: {row['energy_source']}, Energy_source: {row['energy_source']} , Location: {row['location']}"
+    "weather.csv": lambda row: f"Timestamp: {row['timestamp']}, Location: {row['location']}, Temperature: {row['temperature']},Humidity: {row['humidity']}, Solar_irradiance: {row['solar_irradiance']}",
+    "building_information.csv": lambda row: f"Building_id: {row['building_id']}, Building_name: {row['building_name']},Location: {row['location']}, Building_type: {row['building_type']},Square_footage:{row['square_footage']} "
 }
 
 for file, formatter in datasets.items():
